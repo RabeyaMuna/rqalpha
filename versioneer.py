34e0f5b278d873d6d10876428702510ecf858ec4
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-25 23:02:50 -0400

    RabeyaMuna__diff__id_289

--- versioneer.py
+++ versioneer.py
@@ -317,7 +317,13 @@ import sys
 import functools
 
 
-from pkg_resources import parse_version
+try:
+    from packaging.version import parse as parse_version
+except ImportError:
+    try:
+        from setuptools._vendor.packaging.version import parse as parse_version
+    except ImportError:
+        from pkg_resources import parse_version
 
 have_tomllib = True
 if sys.version_info >= (3, 11):
